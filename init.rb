b9811d3b14c944dea8116fd4b339524a7569d371
Marcus Ilgner <mail@marcusilgner.com>
2010-03-01 23:50:57 +0100

    committed remaining files (still not fully accustomed to git)

--- init.rb
+++ init.rb
@@ -5,4 +5,9 @@ Redmine::Plugin.register :redmine_ganttproject_sync do
   author 'Marcus Ilgner <mail@marcusilgner.com>'
   description 'Synchronizes a Redmine issue tracker with data from a GanttProject file'
   version '0.0.1'
+  
+  project_module :ganttproject_sync do
+    permission :synchronize_issues, :sync => [:sync, :import, :apply]
+  end
+  menu :project_menu, :ganttproject_sync, { :controller => 'sync', :action => 'sync' }, :caption => 'Ganttproject', :after => :issues, :param => :project_id
 end
